751bceebb3ea0356d53ab8e5aef3dd4dbdc59f0c
Lyonel Zamora <lyonelz516@gmail.com>
2020-09-13 16:03:43 -0400

    Add Intro Scene and Menu

--- Honey-I'm-Home/Assets/Scripts/SimpleStart.cs
+++ Honey-I'm-Home/Assets/Scripts/SimpleStart.cs
@@ -4,40 +4,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SimpleStart : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void startGame()
     {
-        switch (SceneManager.GetActiveScene().name) {
-            case "Intro":
-                FindObjectOfType<AudioManager>().Play("intro");
-                break;
-            case "Fail":
-                FindObjectOfType<AudioManager>().Play("fail");
-                break;
-            case "Succeed":
-                FindObjectOfType<AudioManager>().Play("succeed");
-                break;
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown("space"))
-        {
-            switch (SceneManager.GetActiveScene().name)
-            {
-                case "Intro":
-                    FindObjectOfType<AudioManager>().Stop("intro");
-                    break;
-                case "Fail":
-                    FindObjectOfType<AudioManager>().Stop("fail");
-                    break;
-                case "Succeed":
-                    FindObjectOfType<AudioManager>().Stop("succeed");
-                    break;
-            }
-            SceneManager.LoadScene("Main");
-        }
+        SceneManager.LoadScene("Intertitle");
     }
 }
