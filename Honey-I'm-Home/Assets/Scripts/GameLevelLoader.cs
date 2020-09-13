82d5919d669580747d8a42867e7647dc707323d9
Lyonel Zamora <lyonelz516@gmail.com>
2020-09-13 17:12:08 -0400

    Yeah

---
+++ Honey-I'm-Home/Assets/Scripts/GameLevelLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameLevelLoader : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(loadGame());
+    }
+
+    IEnumerator loadGame()
+    {
+        yield return new WaitForSeconds(18);
+        SceneManager.LoadScene("Main");
+    }
+}
