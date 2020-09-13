6d3d88d663c78f1cd92d7d0511215b2dd7e98a74
Lyonel Zamora <lyonelz516@gmail.com>
2020-09-13 12:03:50 -0400

    Add PainOverlay and Script Controller

---
+++ Honey-I'm-Home/Assets/Scripts/PainOverlayController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PainOverlayController : MonoBehaviour
+{
+    public void adjustOpacity(float painMeter)
+    {
+        float painPercentage = painMeter / 100;
+
+        SpriteRenderer overlayRenderer = GetComponent<SpriteRenderer>();
+        Color newColor = new Color(overlayRenderer.color.r, overlayRenderer.color.g, overlayRenderer.color.r, painPercentage);
+        overlayRenderer.color = newColor;
+    }
+}
